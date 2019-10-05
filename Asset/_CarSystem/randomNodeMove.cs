002e2545b78ef389795afe01f0155dae6683c940
jsa9292 <jsa9292@gmail.com>
2019-10-04 22:58:33 -0700

    Add files via upload

---
+++ Asset/_CarSystem/randomNodeMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class randomNodeMove : MonoBehaviour {
+    public float timer;
+    public Transform[] Nodes;
+    public int destPoint = 0;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+        timer += Time.deltaTime;
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
